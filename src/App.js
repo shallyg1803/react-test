b10ec7d4b3cc2bf6cafb343ee2641a5e32182f1e
Shally Gupta <shally_g1803@yahoo.com>
2019-08-30 13:03:15 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Header from './Header';
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    // let x=1;
+    // let y=2;
+    return (
       <div>
-        Phone Directory
+        <Header />
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
       </div>
+ //   <label htmlFor="name">Name:</label>
+      //   <input type="text" id="name" placeholder="Type Here" defaultValue={x+y} />
+    
+      // <div>
+      //   <p>ReactJs</p>
+      // </div>
+      // React.createElement("div", null,
+      // React.createElement("p",null, "ReactJs")
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = function(){
+    return(
+        <div className="heading">
+            Phone Directory
+        </div>
+    )
+}
+
+export default Header;

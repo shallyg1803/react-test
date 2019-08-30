eccba61a6fb66097eca41531df6e2b158904edae
Shally Gupta <shally_g1803@yahoo.com>
2019-08-30 19:38:42 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ class App extends Component {
   render() {
     // let x=1;
     // let y=2;
+    let Subscribers = [
+      {
+        id: 1,
+        name: "Shally",
+        phone: "9999999999"
+      },
+      {
+        id: 2,
+        name: "ShallyG",
+        phone: "9999999999"
+      }
+    ]
     return (
       <div>
         <Header />
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+        <div  className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span><br />
+          <span className="grid-item phone-heading">Phone</span>        
+        </div>
+        {
+          Subscribers.map(sub =>{
+            return <div key={sub.id} className="grid-container">
+            <span className="grid-item">{sub.name}</span><br />
+            <span className="grid-item">{sub.phone}</span><br />
+          </div>
+          })
+        }
+       
         </div>
       </div>
  //   <label htmlFor="name">Name:</label>
@@ -23,6 +46,7 @@ class App extends Component {
       // React.createElement("div", null,
       // React.createElement("p",null, "ReactJs")
     );
+  
   }
 }
 
